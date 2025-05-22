bf5d1785efde82925ff7d58c6ca8b32cab770e86
cyberdeeb <abrahamdeeb@gmail.com>
2025-05-22 13:27:54 -0700

    Create app.js with event listener and fetch function

---
+++ app.js
@@ -0,0 +1,47 @@
+const apiKey = 'API_KEY';
+const weatherButton = document.getElementById('weatherButton');
+const locationInput = document.getElementById('locationInput');
+const loading = document.getElementById('loading');
+const resultDiv = document.getElementById('result');
+const errorDiv = document.getElementById('error');
+
+weatherButton.addEventListener('click', async() => {
+  const location = locationInput.trim();
+
+  if (!location) return;
+
+  resultDiv.classList.add('hidden');
+  errorDiv.classList.add('hidden');
+  loading.classList.remove('hidden');
+
+  try {
+    const response = await fetch(
+            `http://api.weatherstack.com/current?access_key=${apiKey}&query=${location}`
+    );
+
+    if (!response.ok) {
+      throw new Error('City not found');
+    }
+
+    const data = await response.json();
+    const outputLocation = data.location.name;
+    const temp = data.current.temperature;
+    const description = data.current.weather_description;
+    const icon = data.current.weather_icons[0];
+
+    resultDiv.innerHTML = `
+        <h2>${outputLocation}</h2>
+        <p>${description}</p>
+        <p>${temp}&deg;F</p>
+        <img src="${icon}" />
+
+        `;
+
+    resultDiv.classList.remove('hidden');
+  } catch (err) {
+    errorDiv.textContent = err.message;
+    errorDiv.classList.remove('hidden');
+  } finally {
+    loading.classList.add('hidden');
+  }
+});
